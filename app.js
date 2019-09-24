8665c7095801292cd8e0c01648d94d617282a540
Germain SIPIERE <gsipiere@gmail.com>
2019-09-24 10:58:49 +0200

    favicon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const logo = document.querySelectorAll("#logo path");
-
+// calcul des longueurs de chemins
 for(let i = 0; i<logo.length;i++){
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
